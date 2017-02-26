96f5b9c4a4a1ece6626ebed1b52d0f7a3a4f5ad6
Jakubinyi <jakubinyi@gmail.com>
2017-02-26 17:35:02 +0100

    3.Task added

---
+++ src/bolt/FeltartosTej.java
@@ -0,0 +1,11 @@
+package bolt;
+
+import java.util.Date;
+
+public class FeltartosTej extends Tej {
+
+	public FeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagido, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagido, zsirtartalom);
+	}
+
+}
---
+++ src/bolt/TartosTej.java
@@ -0,0 +1,11 @@
+package bolt;
+
+import java.util.Date;
+
+public class TartosTej extends Tej {
+
+	public TartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagido, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagido, zsirtartalom);
+	}
+
+}
