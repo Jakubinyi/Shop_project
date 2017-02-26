3bc7b6fdf1165e3dc47a203527efeb0c018e72d9
Jakubinyi <jakubinyi@gmail.com>
2017-02-26 14:27:44 +0100

    1.Task added

---
+++ src/bolt/Tej.java
@@ -0,0 +1,61 @@
+package bolt;
+
+import java.util.Date;
+
+public class Tej {
+	
+	private int urtartalom;
+	private String gyarto;
+	private Date szavatossagido;
+	private double zsirtartalom;
+	private long ar;
+	
+	public Tej(int urtartalom, String gyarto, Date szavatossagido, double zsirtartalom, long ar) {
+		this.urtartalom = urtartalom;
+		this.gyarto = gyarto;
+		this.szavatossagido = szavatossagido;
+		this.zsirtartalom = zsirtartalom;
+		this.ar = ar;
+	}
+
+	public boolean joMeg(){
+		Date currentDate = new Date();
+		if(this.szavatossagido.compareTo(currentDate) > 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public int getUrtartalom() {
+		return urtartalom;
+	}
+
+	public String getGyarto() {
+		return gyarto;
+	}
+
+	public Date getSzavatossagido() {
+		return szavatossagido;
+	}
+
+	public double getZsirtartalom() {
+		return zsirtartalom;
+	}
+
+	public long getAr() {
+		return ar;
+	}
+
+	@Override
+	public String toString() {
+		return "Tej [urtartalom=" + urtartalom + 
+				", gyarto=" + gyarto + 
+				", szavatossagido=" + szavatossagido+ 
+				", zsirtartalom=" + zsirtartalom + 
+				", ar=" + ar + "]";
+	}
+	
+	
+	
+	
+}
