14449830991056975956fe2ac6117471ac557e75
Jakubinyi <jakubinyi@gmail.com>
2017-02-26 22:42:53 +0100

    5.1.Task

--- src/bolt/BoltFactory.java
+++ src/bolt/BoltFactory.java
@@ -2,11 +2,13 @@ package bolt;
 
 import java.util.Date;
 
+import bolt.aruk.Sajt;
+import bolt.aruk.Szappan;
 import bolt.aruk.Tej;
 import bolt.aruk.tej.FeltartosTej;
 import bolt.aruk.tej.TartosTej;
 
-public abstract class Tejfactory {
+public abstract class BoltFactory {
 
 	public Tej ujTartosTej(Long vonalKod, String gyarto, Date szavatossagiIdo, int urtartalom, double zsirtartalom) {
 		return new TartosTej(vonalKod,  gyarto, szavatossagiIdo, urtartalom, zsirtartalom);
@@ -14,5 +16,13 @@ public abstract class Tejfactory {
 	
 	public Tej ujFeltosTej(Long vonalKod, String gyarto, Date szavatossagiIdo, int urtartalom, double zsirtartalom) {
 		return new FeltartosTej(vonalKod, gyarto, szavatossagiIdo, urtartalom, zsirtartalom);
+	}
+	
+	public Sajt ujSajt(Long vonalKod, String gyarto, Date szavatossagiIdo, double suly, double zsirtartalom) {
+		return new Sajt(vonalKod, gyarto, szavatossagiIdo, suly, zsirtartalom);
+	}
+	
+	public Szappan ujSzappan(Long vonalKod, String gyarto, char mosohatas) {
+		return new Szappan(vonalKod, gyarto, mosohatas);
 	}
 }
